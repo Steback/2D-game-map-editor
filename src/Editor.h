5dde688e022d2c2ff1c06f2e5c8fc8a20d7516ad
Jesus Guio <jfranciscoguio@gmail.com>
2020-04-10 18:14:15 -0500

    Implement example in Editor Class

--- src/Editor.h
+++ src/Editor.h
@@ -1,12 +1,40 @@
 #ifndef EDITOR_H
 #define EDITOR_H
 
+// Dear ImGui
+#include "../lib//imgui/imgui.h"
+#include "../lib/imgui_glfw/imgui_impl_glfw.h"
+#include "../lib//imgui/imgui_impl_opengl3.h"
+
+// OpenGL Math(GLM)
+#include "../lib/glm/glm.hpp"
+#include "../lib/glm/gtc/matrix_transform.hpp"
+#include "../lib/glm/gtc/type_ptr.hpp"
+
+// OpenGL loader
+#include <GL/glew.h>
+
+// Include glfw3.h after our OpenGL definitions
+#include <GLFW/glfw3.h>
+
 class Editor {
     public:
-        Editor();
+        Editor(ImGuiIO& _io);
         ~Editor();
+        bool isRunning();
+        void initialized();
+        void processInput();
+        void renderUI();
+        void render();
+        void destroy();
 
     private:
+        bool editorIsRunning;
+
+        // Create window with graphics context
+        GLFWwindow* window;
+
+        ImGuiIO io;
 
 };
 
