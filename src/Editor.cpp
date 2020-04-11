d8c8b56e4e411c1b05999631e478bce092ba2c7d
Jesus Guio <jfranciscoguio@gmail.com>
2020-04-11 16:24:42 -0500

    Main menu bar created

--- src/Editor.cpp
+++ src/Editor.cpp
@@ -215,6 +215,48 @@ void Editor::renderUI() {
     ImGui_ImplGlfw_NewFrame();
     ImGui::NewFrame();
 
+    if ( ImGui::BeginMainMenuBar() ) {
+        if ( ImGui::BeginMenu("File") ) {
+            if (ImGui::MenuItem("New")) {}
+            if (ImGui::MenuItem("Open", "Ctrl+O")) {}
+            if (ImGui::MenuItem("Save", "Ctrl+S")) {}
+            if (ImGui::MenuItem("Save As..")) {}
+
+            ImGui::Separator();
+            if ( ImGui::BeginMenu("Options") )
+            {
+                static bool enabled = true;
+                ImGui::MenuItem("Enabled", "", &enabled);
+                ImGui::BeginChild("child", ImVec2(0, 60), true);
+                for (int i = 0; i < 10; i++)
+                    ImGui::Text("Scrolling Text %d", i);
+                ImGui::EndChild();
+                static float f = 0.5f;
+                static int n = 0;
+                ImGui::SliderFloat("Value", &f, 0.0f, 1.0f);
+                ImGui::InputFloat("Input", &f, 0.1f);
+                ImGui::Combo("Combo", &n, "Yes\0No\0Maybe\0\0");
+                ImGui::EndMenu();
+            }
+
+            if ( ImGui::MenuItem("Quit", "Alt+F4") ) { editorIsRunning = false; }
+
+            ImGui::EndMenu();
+        }
+
+        if ( ImGui::BeginMenu("Edit") ) {
+            if (ImGui::MenuItem("Undo", "CTRL+Z")) {}
+            if (ImGui::MenuItem("Redo", "CTRL+Y", false, false)) {}  // Disabled item
+            ImGui::Separator();
+            if (ImGui::MenuItem("Cut", "CTRL+X")) {}
+            if (ImGui::MenuItem("Copy", "CTRL+C")) {}
+            if (ImGui::MenuItem("Paste", "CTRL+V")) {}
+            ImGui::EndMenu();
+        }
+
+        ImGui::EndMainMenuBar();
+    }
+
     // Create a window called "Hello, world!" and append into it.
     ImGui::Begin("Version of GLFW, GLSL and OpenGL");
 
